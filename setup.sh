474a61fb7a1408664f0be544b4d39645e6d68d4f
hayupadhyaya <41875548+hayupadhyaya@users.noreply.github.com>
2024-06-05 18:45:21 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-cd ~/
-sudo apt update
-sudo apt upgrade -y
-sudo apt install speedtest-cli -y
+
+# Update package list
+sudo apt update -y 
+
+# Install XFCE4, XRDP, and UFW
+sudo apt install xfce4 xrdp ufw -y
+
+# Enable and start XRDP service
+sudo systemctl enable xrdp.service
+sudo systemctl start xrdp.service
+
+# Add XRDP user to ssl-cert group
+sudo adduser xrdp ssl-cert
+
+# Allow port 3389 through the firewall
+sudo ufw allow 3389
+
+# Install Tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
-echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf
-echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf
-sudo sysctl -p /etc/sysctl.conf
-sudo tailscale up --advertise-exit-node
+
+# Start Tailscale service
+sudo systemctl start tailscaled
+
+# Configure Tailscale
+sudo tailscale up --qr
